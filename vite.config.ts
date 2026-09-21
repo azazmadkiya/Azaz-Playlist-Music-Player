aa6e7e548b38c0a0eb73149e6be9a8cde92f64f2
Azazmadkiya <74822032+azazmadkiya@users.noreply.github.com>
2026-09-21 21:09:18 +0530

    Commit message

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Azaz-Playlist-Music-Player/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
